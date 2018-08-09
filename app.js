070394c2a99814d4bb13b61001c7aa2af88fe3d0
James <jameswrightfilm@gmail.com>
2018-08-09 09:36:26 +0100

    Add categories and logic

--- app.js
+++ app.js
@@ -1,71 +1,99 @@
 //API KEY 380ab818f47142c6a0b58b098c8a2c02
+let url = 'https://newsapi.org/v2/top-headlines?' +
+    'country=us&' +
+    'apiKey=380ab818f47142c6a0b58b098c8a2c02';
 
-var url = 'https://newsapi.org/v2/top-headlines?' +
-          'country=us&' +
-          'apiKey=380ab818f47142c6a0b58b098c8a2c02';
 
+const buttons = document.getElementById('buttons');
 const articleContainer = document.getElementById('articles');
 
-var req = new Request(url);
-fetch(req)
-    .then(function(response) {return response.json()})
-    .then(function(data){
-    	console.log(data);
-
-    	for(let i = 0; i < 20; i++){
-
-    	let articles = data.articles;
-    	let articleAuthor = articles[i].author;
-    	let articleTitle = articles[i].title;
-    	let articleURL = articles[i].url;
-    	let articleImgURL = articles[i].urlToImage;
-
-		//Create Rows
-		option = articleContainer.appendChild(document.createElement("div"));
-		option.className =  "grid__row";
-		
-		//Create Columns
-		col = option.appendChild(document.createElement('div'));
-		col.className = "grid__col--7 theme__colors centered"
-
-		//Create Headline 
-		headline = col.appendChild(document.createElement('h4'));
-		headline.className = "article--title";
-		headline.textContent = articleTitle;
-
-		//Create Image
-
-		image = col.appendChild(document.createElement('img'));
-		image.className = "article--image";
-		image.src = articleImgURL;
-
-		//hello
 
 
 
-	
- 
- 	   }
-    });
+buttons.addEventListener('click', function(e) {
 
-// const func = () =>{
+    let query = null;
 
-// 	for(let i= 0; i < 20; i++){
+    if (e.target.tagName == 'BUTTON') {
+        if (e.target.innerHTML == 'World') {
+            query = 'q=world&'
+            console.log(e.target)
+        } else if (e.target.innerHTML == 'Movies') {
+            query = 'q=movies&'
 
-// 	//Create Rows
-// 	option = articleContainer.appendChild(document.createElement("div"));
-// 	option.className =  "grid__row";
-	
-// 	//Create Columns
-// 	col = option.appendChild(document.createElement('div'));
-// 	col.className = "grid__col--7 theme__colors centered"
+        } else if (e.target.innerHTML == 'Tech') {
+            query = 'q=tech&'
 
-// 	//Create Headline 
-// 	headline = col.appendChild(document.createElement('h4'));
+        } else if (e.target.innerHTML == 'Gaming') {
+            query = 'q=games&'
 
+        }
+
+    }
 
 
-// 	}
-// }
 
-// func();
+    let url = 'https://newsapi.org/v2/top-headlines?' +
+        query +
+        'apiKey=380ab818f47142c6a0b58b098c8a2c02';
+
+    console.log(url)
+
+    // beginFetch(url)
+
+
+})
+
+
+
+
+//a is URL
+const beginFetch = (a) => {
+
+    var req = new Request(a);
+
+
+    fetch(req)
+        .then(function(response) {
+            return response.json()
+        })
+        .then(function(data) {
+            console.log(data);
+
+            while (articleContainer.firstChild) {
+                articleContainer.removeChild(articleContainer.firstChild);
+            }
+            for (let i = 0; i < 20; i++) {
+
+                let articles = data.articles;
+                let articleAuthor = articles[i].author;
+                let articleTitle = articles[i].title;
+                let articleURL = articles[i].url;
+                let articleImgURL = articles[i].urlToImage;
+
+
+                //Create Rows
+                option = articleContainer.appendChild(document.createElement("div"));
+                option.className = "grid__row";
+
+                //Create Columns
+                col = option.appendChild(document.createElement('div'));
+                col.className = "grid__col--7 theme__colors centered"
+
+                //Create Headline 
+                headline = col.appendChild(document.createElement('h4'));
+                headline.className = "article--title";
+                headline.textContent = articleTitle;
+
+                //Create Image
+                image = col.appendChild(document.createElement('img'));
+                image.className = "article--image";
+                image.src = articleImgURL;
+
+                console.log('Fetch Run with URL' + url)
+
+            }
+        });
+}
+
+// beginFetch(url);
